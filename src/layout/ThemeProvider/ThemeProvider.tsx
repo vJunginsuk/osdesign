94ed41893f53f70c59db3bd1fd9561d9e877bcb7
junginsuk <jis2392@opensg.net>
2024-04-03 14:44:22 +0900

    theme localStorage에 저장

--- src/layout/ThemeProvider/ThemeProvider.tsx
+++ src/layout/ThemeProvider/ThemeProvider.tsx
@@ -5,18 +5,24 @@ import { themeProps } from '../../interfaces/props.interface';
 
 const ThemeProvider = (props: themeProps) => {
   const { children } = props;
-  const [theme, setTheme] = useState<string>('');
+  const localTheme: any = localStorage.getItem('THEME');
+  const [theme, setTheme] = useState<string>(
+    localTheme !== null ? localTheme : 'light',
+  );
 
   const onClickTheme = useCallback(() => {
-    if (theme === 'light' || theme === '') {
+    if (theme === 'light') {
       setTheme('dark');
+      localStorage.setItem('THEME', 'dark');
     } else if (theme === 'dark') {
       setTheme('light');
+      localStorage.setItem('THEME', 'light');
     }
   }, [theme]);
 
   const onChangeTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTheme(event.target.value);
+    localStorage.setItem('THEME', event.target.value);
   };
 
   return (
