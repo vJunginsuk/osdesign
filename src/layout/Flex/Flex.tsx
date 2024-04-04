b229877122bb5ccfe3b01ba5186b29caebdad7e8
junginsuk <jis2392@opensg.net>
2024-04-04 15:51:11 +0900

    함수명 변경

--- src/layout/Flex/Flex.tsx
+++ src/layout/Flex/Flex.tsx
@@ -2,12 +2,19 @@ import classNames from 'classnames';
 import React from 'react';
 import { IFlexProps } from '../../interfaces/props.interface';
 
-const index = (props: IFlexProps) => {
-  const { children, style, vertical, wrap, className, align, justify, gap } = props;
-  const classes = classNames('os-flex', vertical && 'os-flex-vertical', wrap && 'os-flex-wrap', className, {
-    [`os-flex-align-${align}`]: align,
-    [`os-flex-justify-${justify}`]: justify,
-  });
+const Flex = (props: IFlexProps) => {
+  const { children, style, vertical, wrap, className, align, justify, gap } =
+    props;
+  const classes = classNames(
+    'os-flex',
+    vertical && 'os-flex-vertical',
+    wrap && 'os-flex-wrap',
+    className,
+    {
+      [`os-flex-align-${align}`]: align,
+      [`os-flex-justify-${justify}`]: justify,
+    },
+  );
   const styles = {
     gap: gap,
     ...style,
@@ -20,4 +27,4 @@ const index = (props: IFlexProps) => {
   );
 };
 
-export default index;
+export default Flex;
