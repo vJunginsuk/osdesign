aab0994ebde8d8b92fdda6f544c6da8fd24379c1
junginsuk <jis2392@opensg.net>
2024-04-04 09:38:47 +0900

    사용안하는 태그 삭제

--- src/layout/ThemeProvider/ThemeProvider.tsx
+++ src/layout/ThemeProvider/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { ThemeContext } from '../../stores/context';
 import { themeProps } from '../../interfaces/props.interface';
